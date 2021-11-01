fee07b07dc7497b325d383961d01255258828f11
Webverse <32201875+thewebverse@users.noreply.github.com>
2021-11-01 09:58:20 +0100

    Update main.js

--- view/frontend/web/js/main.js
+++ view/frontend/web/js/main.js
@@ -2,95 +2,18 @@
 
 require(['jquery'], function ($) {
 
-    // Prevent opening contextmenu, which is opened by right click
+    // Disable Contextmenu, Disable Right Click;
     document.addEventListener('contextmenu', event => event.preventDefault());
 
-    // 17 -> ctrl key
-    document.onkeypress = function (e) {
-        e = e || window.event;
-        if (e.keyCode === 17) {
-            return false;
-        }
-    };
-
-    // 17 -> ctrl key
-    window.onkeyup = function (e) {
-        if (e.which === 17) n = false;
-    };
-
-    // 123 -> f12 key
-    document.onkeypress = function (e) {
-        e = e || window.event;
-        if (e.keyCode === 123) {
-            return false;
-        }
-    };
-
-    // 123 -> f12 key
-    document.onmousedown = function (e) {
-        e = e || window.event;
-        if (e.keyCode === 123) {
-            return false;
-        }
-    };
-
-    // 123 -> f12 key
-    document.onkeydown = function (e) {
-        e = e || window.event;
-        if (e.keyCode === 123) {
-            return false;
-        }
-    };
-
-    // 17 -> ctrl key
-    // 65 -> a key
-    // 67 -> c key
-    // 74 -> j key
-    // 83 -> s key
-    // 85 -> u key
-    // 86 -> v key
-    // 88 -> x key
-    window.onkeydown = function (e) {
-        if (e.which === 17) n = true;
-        if ((e.which === 85 || e.which === 65 || e.which === 74 || e.which === 88 || e.which === 67 || e.which === 86 || e.which === 83) && n === true) {
-            return false;
-        }
-    };
-
-    // 91 and 93 are the Windows Keys or Command Key
-    window.onkeydown = function (e) {
-        if (e.which === 91) n = true;
-        if ((e.which === 85 || e.which === 65 || e.which === 74 || e.which === 88 || e.which === 67 || e.which === 86 || e.which === 83) && n === true) {
-            return false;
-        }
-    };
-    window.onkeydown = function (e) {
-        if (e.which === 93) n = true;
-        if ((e.which === 85 || e.which === 65 || e.which === 74 || e.which === 88 || e.which === 67 || e.which === 86 || e.which === 83) && n === true) {
-            return false;
-        }
-    };
-
-    // Command Option C on mac inspect elements
-    window.onkeydown = function (e) {
-        if (e.which === 91) n = true;
-        if (e.which === 18) n = true;
-        if ((e.which === 85 || e.which === 65 || e.which === 74 || e.which === 88 || e.which === 67 || e.which === 86 || e.which === 83) && n === true) {
-            return false;
-        }
-    };
-    window.onkeydown = function (e) {
-        if (e.which === 93) n = true;
-        if (e.which === 18) n = true;
-        if ((e.which === 85 || e.which === 65 || e.which === 74 || e.which === 88 || e.which === 67 || e.which === 86 || e.which === 83) && n === true) {
-            return false;
-        }
-    };
-
-    $(document).keydown(function (event) {
-        if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
-            return false;
+    document.addEventListener("keydown", function (event) {
+        if (event.ctrlKey) {
+            // Disable CTRL -> CTRL+C, CTRL+V, CTRL+A, CTRL+SHIFT+I, CTRL+U
+            event.preventDefault();
+        } else if (event.keyCode === 123 || event.keyCode === 91 || event.keyCode === 93) {
+            // Disable F12 || Windows Key and Command Key
+            event.preventDefault();
         }
     });
+
 
 });
